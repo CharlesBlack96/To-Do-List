d84ac4f7bd4608e987c214ee80a9c5f8fea8816c
Charlie Black <charlieblack@Larrys-MacBook-Pro.local>
2022-06-29 17:11:41 -0700

    first commit

---
+++ src/list.js
@@ -0,0 +1,29 @@
+[
+	{ 
+	  id: 23
+      todo: "wake up 6am"
+
+	}, 
+
+	{
+		id: 12,
+		todo: "brush teeth"
+	
+	},
+
+    {
+        id:75
+        todo: "feed cats"
+    },
+
+    {
+        id:92
+        todo: "make smoothie"
+    },
+
+    {
+        id:67
+        todo: "make todo list for the day"
+    }
+
+]
